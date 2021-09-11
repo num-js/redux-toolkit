e4dfbc7485ad72d06121c89d482d3e3caf0e93f5
Numan Ahmed <mdnmnahmed@gmail.com>
2021-09-11 10:56:40 +0530

    Create Actions in new Redux-ToolKit Style

--- src/Store/actions/userAction.js
+++ src/Store/actions/userAction.js
@@ -1,10 +1,16 @@
+import { createAction } from "@reduxjs/toolkit";
 import { UPDATE_PROFESSION } from "../constants"
 
-const updateProfession = (updatedData) => {
-    return {
-        type: UPDATE_PROFESSION,
-        payload: updatedData
-    }
-}
+// In Old Redux Style
+// const updateProfession = (updatedData) => {
+//     return {
+//         type: UPDATE_PROFESSION,
+//         payload: updatedData
+//     }
+// }
+
+
+// In new Redux-ToolKit Style
+const updateProfession = createAction(UPDATE_PROFESSION);
 
 export default updateProfession;
