f13f19aea80152a5453d4859ee4d179d06c3a899
Numan Ahmed <mdnmnahmed@gmail.com>
2021-09-11 09:10:16 +0530

    Configure Redux Store

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const initialState = {
+    name: 'Numan',
+    profession: 'SD1',
+    status: 'Single'
+}
+
+const Store = configureStore({
+    reducer: (state) => {
+        return state;
+    },
+    preloadedState: initialState
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={Store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
